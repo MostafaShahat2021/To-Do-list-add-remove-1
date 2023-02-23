9e65c81b2652b798802ea3b428a7f59fd0e2713b
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2023-02-23 17:31:59 +0200

    changes required by the code reviewer

--- modules/interactive-list.js
+++ modules/interactive-list.js
@@ -1,15 +1,11 @@
-import { tasks, addElemToPage } from './toDos.js';
+import { tasks } from './toDos.js';
 
 // ======Checked Function=======
 window.updateStatus = (selectedTask) => {
-  // console.log(selectedTask);
   const taskName = selectedTask.nextElementSibling;
-  // console.log(taskName);
   if (selectedTask.checked) {
     taskName.classList.add('checked');
-    // console.log(tasks[selectedTask.id].completed);
     tasks[selectedTask.id - 1].completed = true;
-    // console.log(tasks[selectedTask.id].completed = true);
   } else {
     taskName.classList.remove('checked');
     tasks[selectedTask.id - 1].completed = false;
@@ -21,10 +17,10 @@ window.updateStatus = (selectedTask) => {
 const clearCompletedBtn = document.querySelector('.clear');
 
 clearCompletedBtn.addEventListener('click', () => {
-  tasks = tasks.filter((task) => !task.completed);
-  tasks.forEach((e, i) => {
+  const completed = tasks.filter((task) => !task.completed);
+  completed.forEach((e, i) => {
     e.index = i + 1;
   });
-  localStorage.setItem('task-list', JSON.stringify(tasks));
-  addElemToPage();
+  localStorage.setItem('task-list', JSON.stringify(completed));
+  window.location.reload();
 });
